e1282260f0482ea668a327d8a95e19ba6deccf97
Gleici Pereira <gpcaresdev@gmail.com>
2022-03-28 09:59:50 -0500

    carousel details, pictures

--- src/App.js
+++ src/App.js
@@ -1,145 +1,151 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from 'react-bootstrap/Carousel';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import './index.css';
 
 
 
 function App() {
   return (
     <div className="App">
+    
       <Carousel fade variant='dark'>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/ecoplay-07.jpg')}
             alt="Ecoplay Logo" />
           <Carousel.Caption>
-            <h3>Ecoplay</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>ECOPLAY</h3>
+            <p>Business cards</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/ecoplay-13.jpg')}
             alt="Ecoplay hat design" />
           <Carousel.Caption>
-            <h3>Ecoplay</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>ECOPLAY</h3>
+            <p>Design promotional giveways</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/ecoplay-03.jpg')}
             alt="Ecoplay Social Media" />
           <Carousel.Caption>
-            <h3>Ecoplay</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>ECOPLAY</h3>
+            <p>Social media advertising</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/mastercakes-03.jpg')}
             alt="Master Cakes Facade" />
           <Carousel.Caption>
-            <h3>Master Cakes</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>MASTER CAKES</h3>
+            <p>Visual communication</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
-            src={require('./images/mastercakes-06.jpg')}
+            style={{ height: '100%', width: '100%' }}
+            src={require('./images/mastercakes-test.jpg')}
             alt="Master Cakes Webpage" />
           <Carousel.Caption>
-            <h3>Master Cakes</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>MASTER CAKES</h3>
+            <p>Web design and social media management</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/mastercakes-01.jpg')}
-            alt="Master Cakes advertising"/>
+            alt="Master Cakes advertising" />
           <Carousel.Caption>
-            <h3>Master Cakes</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>MASTER CAKES</h3>
+            <p>Product packaging design</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/sonho-magico-07.jpeg')}
             alt="Sonho Magico Embalagens" />
           <Carousel.Caption>
-            <h3>Sonho Mágico</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>SONHO MÁGICO</h3>
+            <p>Visual identity</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/sonho-magico-12.png')}
             alt="Sonho Magico cups" />
           <Carousel.Caption>
-            <h3>Sonho Mágico</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>SONHO MÁGICO</h3>
+            <p>Product packaging design</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
-            src={require('./images/sonho-magico-06.png')}
+            style={{ height: '100%', width: '100%' }}
+            src={require('./images/sonho-magico-test.jpg')}
             alt="Sonho Magico cards" />
           <Carousel.Caption>
-            <h3>Sonho Mágico</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>SONHO MÁGICO</h3>
+            <p>Business cards</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/pedra-nobre-01.jpg')}
-            alt="Pedra Nobre Marmoraria"/>
+            alt="Pedra Nobre Marmoraria" />
           <Carousel.Caption>
-            <h3>Pedra Nobre Marmoraria</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>PEDRA NOBRE</h3>
+            <p>Visual identity</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/pedra-nobre-07.jpg')}
-            alt="Pedra Nobre"/>
+            alt="Pedra Nobre" />
           <Carousel.Caption>
-            <h3>Pedra Nobre</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>PEDRA NOBRE</h3>
+            <p>Design promotional giveways</p>
           </Carousel.Caption>
-          </Carousel.Item>
-        
+        </Carousel.Item>
+
         <Carousel.Item>
           <img
             className="d-block"
-            style={{height: '100%', width:'100%'}}
+            style={{ height: '100%', width: '100%' }}
             src={require('./images/pedra-nobre-10.jpg')}
-            alt="Pedra Nobre Vehicles"/>
+            alt="Pedra Nobre Vehicles" />
           <Carousel.Caption>
-            <h3>Pedra Nobre Marmoraria</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h3>PEDRA NOBRE</h3>
+            <p>Custom fleet graphics & Wraps</p>
           </Carousel.Caption>
 
         </Carousel.Item>
